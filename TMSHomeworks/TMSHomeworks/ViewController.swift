bb36aed93e0cd5d6a7f88862dd40dd22ab2e20bb
Aliaksandr Liakh <lav_by@icloud.com>
2023-11-27 15:39:01 +0200

    First homework commitment

--- TMSHomeworks/TMSHomeworks/ViewController.swift
+++ TMSHomeworks/TMSHomeworks/ViewController.swift
@@ -11,7 +11,32 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let array = [2.5, 9.7, 6.9, 8.2]
+        var intSum = 0
+        var fractSum = 0
+        
+        for i in array {
+            let eachNumberInt = Int(i)
+            intSum += eachNumberInt
+            let numberString = String(i)
+            let numberComponent = numberString.components(separatedBy :".")
+            let fractionalNumber = Int(numberComponent [1]) ?? 0
+            fractSum += fractionalNumber
+        }
+        print("Сумма целых частей равна \(intSum)")
+        if intSum % 2 == 0 {
+            print("и это число четное")
+        } else {
+            print("и это число не четное")
+        }
+        
+        print("Сумма дробных частей равна \(fractSum)")
+        if fractSum % 2 == 0 {
+            print("и это число четное")
+        } else {
+            print("и это число не четное")
+        }
     }
 
 
